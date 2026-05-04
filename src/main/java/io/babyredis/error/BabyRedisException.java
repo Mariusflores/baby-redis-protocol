bb0a74f671cd1fa569a8da10cbf854bf6ce5e92e
mariusflores <mfloresdal@gmail.com>
2026-05-04 11:03:28 +0200

    added additional constructors to BabyRedisException taking Throwable as param

--- src/main/java/io/babyredis/error/BabyRedisException.java
+++ src/main/java/io/babyredis/error/BabyRedisException.java
@@ -4,4 +4,12 @@ public class BabyRedisException extends RuntimeException {
     public BabyRedisException(String message) {
         super(message);
     }
+
+    public BabyRedisException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BabyRedisException(Throwable cause) {
+        super(cause);
+    }
 }
